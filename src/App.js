0cd10b54174397dbcab76bd8749b832c9aadf9aa
proraj9088 <rp8735685@gmail.com>
2021-04-04 01:41:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          myfacebook project started
+      </header>
+    </div>
+  );
+}
+
+export default App;
