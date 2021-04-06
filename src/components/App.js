54c6c0c549d4b451e33e0203de69a1a5bf2c76c2
proraj9088 <rp8735685@gmail.com>
2021-04-06 22:49:14 +0530

    UI of navbar is created

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
 import posts from '../reducers/posts';
-import {PostsList} from './';
-
+import { PostsList } from './';
 
 class App extends React.Component {
   componentDidMount() {
@@ -15,6 +14,60 @@ class App extends React.Component {
     const { posts } = this.props;
     return (
       <div>
+        <nav className="nav">
+          <div className="left-div">
+            <img
+              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
+              alg="logo"
+            />
+          </div>
+          <div className="search-container">
+            <img
+              className="search-icon"
+              src="https://image.flaticon.com/icons/svg/483/483356.svg"
+              alt="search-icon"
+            />
+            <input placeholder="Search" />
+            <div className="search-results">
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="avatar"
+                  />
+                  <sapn>John Doe</sapn>
+                </li>
+              </ul>
+              <ul>
+                <li className="search-results-row">
+                  <img
+                    src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                    alt="avatar"
+                  />
+                  <sapn>John Doe</sapn>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="right-nav">
+            <div className="user">
+              <img
+                src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+                id="user-dp"
+                alt="user-dp"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="nav-links">
+            <ul>
+              <li>Log in</li>
+              <li>Log out</li>
+              <li>Register</li>
+            </ul>
+          </div>
+          </div>
+         
+        </nav>
         <PostsList posts={posts} />
       </div>
     );
@@ -27,6 +80,6 @@ function mapStateToProps(state) {
   };
 }
 App.propTypes = {
-  posts: PropTypes.array.isRequired
-}
+  posts: PropTypes.array.isRequired,
+};
 export default connect(mapStateToProps)(App);
