a98b24663b2701b5d5e562419e35843d5eb7ce10
proraj9088 <rp8735685@gmail.com>
2021-04-13 12:16:52 +0530

    private settings route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route,Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch, Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
 import { Home, Navbar, Page404, Login,Signup} from './';
 import * as jwtDecode from 'jwt-decode';
 import { authenticateUser } from '../actions/auth';
 
-
+const Settings = () => <div>Setting</div>
+const PrivateRoute = (privateRouteProps) => {
+  const {isLoggedin , path, component: Component} = privateRouteProps;
+  return <Route path={path} render={(props) => {
+     return isLoggedin ? <Component {...props} /> : <Redirect to ="/login" />
+  }} />
+}
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -26,7 +32,7 @@ class App extends React.Component {
   }
 
   render() {
-    const { posts } = this.props;
+    const { posts, auth } = this.props;
     return (
       <Router>
         <div>
@@ -42,6 +48,7 @@ class App extends React.Component {
           />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/settings" component={Settings} isLoggedin={auth.isLoggedin} />
           <Route component={Page404} />
           </Switch>
         </div>
@@ -53,6 +60,7 @@ class App extends React.Component {
 function mapStateToProps(state) {
   return {
     posts: state.posts,
+    auth: state.auth
   };
 }
 App.propTypes = {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -51,11 +51,13 @@ class Navbar extends React.Component {
         <div className="right-nav">
           {auth.isLoggedin && (
             <div className="user">
+              <Link to="/settings">
               <img
                 src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
                 id="user-dp"
                 alt="user-dp"
               />
+              </Link>
               <span>{auth.user.name}</span>
             </div>
           )}
